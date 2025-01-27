85ad0a24309ce00ff8f7e6f31f15876d554f38c7
santhosh-sivkumar <santhosh.sivkumar@gmail.com>
2025-01-27 14:46:49 +0530

    FIXED IMPORTING USEEFFECT

--- src/components/RecipeDetailsPage.jsx
+++ src/components/RecipeDetailsPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import {
